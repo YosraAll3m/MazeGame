cc489c530841cd5d24706ee6afdbcda0e7a11570
YosraAllam <eng.yosra.allam@gmail.com>
2022-06-04 16:01:39 +0200

    BFS mase game

---
+++ main.py
@@ -0,0 +1,179 @@
+import turtle
+import time
+import sys
+from collections import deque
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Maze Game")
+wn.setup(1300,700)
+
+
+class Maze(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.speed(0)
+
+class purple(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("purple")
+        self.penup()
+        self.speed(0)
+
+class Blue(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("blue")
+        self.penup()
+        self.speed(0)
+
+
+class Pink(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("pink")
+        self.penup()
+        self.speed(0)
+
+class Yellow(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("yellow")
+        self.penup()
+        self.speed(0)
+
+
+grid= [
+"+++++++++++++++++++++++++++s++++++++++++++++++++++++",
+"+               +                                 +",
+"+  ++++++++++  +++++++++++++  +++++++  ++++++++++++",
+"+          +                 +               ++  +",
+"+  +++++++  +++++++++++++  +++++++++++++++++++++  +",
+"+  +     +  +           +  +                 +++  +",
+"+  +  +  +  +  +  ++++  +  +  +++++++++++++  +++  +",
+"+  +  +  +  +  +  +        +  +  +        +       +",
+"+  +  ++++  +  ++++++++++  +  +  ++++  +  +  ++   +",
+"+  +     +  +          +   +           +  +  ++  ++",
+"+  ++++  +  +++++++ ++++++++  +++++++++++++  ++  ++",
+"+     +  +     +              +              ++   +",
+"++++  +  ++++++++++ +++++++++++  ++++++++++  +++  +",
+"+  +  +                    +     +     +  +  +++  +",
+"+  +  ++++  +++++++++++++  +  ++++  +  +  +  ++   +",
+"+  +  +     +     +     +  +  +     +     +  ++  ++",
+"+  +  +  +++++++  ++++  +  +  +  ++++++++++  ++  ++",
+"+                       +  +  +              ++  ++",
+"+ ++++++             +  +  +  +  +++        +++  ++",
+"+ ++++++ ++++++ +++++++++    ++ ++   ++++++++++  ++",
+"+ +    +    +++ +     +++++++++ ++  +++++++    + ++",
+"+ ++++ ++++ +++ + +++ +++    ++    ++    ++ ++ + ++",
+"+ ++++    +     + +++ +++ ++ ++++++++ ++ ++ ++   ++",
+"+      ++ +++++e+++++     ++          ++    +++++++",
+"+++++++++++++++++++++++++++++++++++++++++++++++++++",
+ ]
+
+
+def setup_maze(grid):
+    global start_x, start_y, end_x, end_y
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            character = grid[y][x]
+            screen_x = -588 + (x * 24)
+            screen_y = 288 - (y * 24)
+
+            if character == "+":
+                maze.goto(screen_x, screen_y)
+                maze.stamp()
+                walls.append((screen_x, screen_y))
+
+            if character == " " or character == "e":
+                path.append((screen_x, screen_y))
+
+            if character == "e":
+                pink.color("purple")
+                pink.goto(screen_x, screen_y)
+                end_x, end_y = screen_x,screen_y
+                pink.stamp()
+                pink.color("pink")
+
+            if character == "s":
+                start_x, start_y = screen_x, screen_y
+                blue.goto(screen_x, screen_y)
+
+
+def endProgram():
+    wn.exitonclick()
+    sys.exit()
+
+def search(x,y):
+    frontier.append((x, y))
+    solution[x,y] = x,y
+
+    while len(frontier) > 0:
+        time.sleep(0)
+        x, y = frontier.popleft()
+
+        if(x - 24, y) in path and (x - 24, y) not in visited:
+            cell = (x - 24, y)
+            solution[cell] = x, y
+
+            frontier.append(cell)
+            visited.add((x-24, y))
+
+        if (x, y - 24) in path and (x, y - 24) not in visited:
+            cell = (x, y - 24)
+            solution[cell] = x, y
+
+            frontier.append(cell)
+            visited.add((x, y - 24))
+            print(solution)
+
+        if(x + 24, y) in path and (x + 24, y) not in visited:
+            cell = (x + 24, y)
+            solution[cell] = x, y
+            frontier.append(cell)
+            visited.add((x +24, y))
+
+        if(x, y + 24) in path and (x, y + 24) not in visited:
+            cell = (x, y + 24)
+            solution[cell] = x, y
+
+            frontier.append(cell)
+            visited.add((x, y + 24))
+        pink.goto(x,y)
+        pink.stamp()
+
+
+def backRoute(x, y):
+    purple.goto(x, y)
+    purple.stamp()
+    while (x, y) != (start_x, start_y):
+        purple.goto(solution[x, y])
+        purple.stamp()
+        x, y = solution[x, y]
+maze = Maze()
+pink = Pink()
+blue = Blue()
+purple = purple()
+yellow = Yellow()
+
+
+walls = []
+path = []
+visited = set()
+frontier = deque()
+solution = {}
+
+
+# main program starts here ####
+setup_maze(grid)
+search(start_x,start_y)
+backRoute(end_x, end_y)
+wn.exitonclick()
